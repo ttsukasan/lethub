c3093f97e023d4996b029963369bc74059c2bcfe
t.tsukamoto <tmak.tsukamoto@gmail.com>
2023-06-18 14:49:02 +0900

    Revert "HTMLテンプレートを導入"
    This reverts commit 7f59a1785fd20c9bb214d7a45340a4dbafc7bced.

--- js/plugins.js
+++ js/plugins.js
@@ -1,4 +1,3 @@
-
 // Avoid `console` errors in browsers that lack a console.
 (function() {
   var method;
@@ -23,4 +22,3 @@
 }());
 
 // Place any jQuery/helper plugins in here.
-// import 'jquery';
